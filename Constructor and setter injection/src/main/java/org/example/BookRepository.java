591deff043fe4e9fa13c48d9d670a1507454f813
Shimal Akmal D <shimalvip@gmail.com>
2026-07-02 14:35:15 +0530

    handson 14 additional

---
+++ Constructor and setter injection/src/main/java/org/example/BookRepository.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class BookRepository {
+
+    public void displayBook() {
+        System.out.println("Fetching book details...");
+    }
+}
---
+++ Constructor and setter injection/src/main/java/org/example/BookService.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Constructor Injection
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Constructor Injection Executed");
+    }
+
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Setter Injection Executed");
+    }
+
+    public void issueBook() {
+        System.out.println("Book Issued");
+        bookRepository.displayBook();
+    }
+}
